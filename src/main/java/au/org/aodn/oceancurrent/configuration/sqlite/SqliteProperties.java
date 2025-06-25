df2abc060b7c8c5beac22b45cada1274c50954b0
Weite Dai <weite.dai@utas.edu.au>
2025-06-25 15:44:41 +1000

    feat: add sqlite configuration and db connection

---
+++ src/main/java/au/org/aodn/oceancurrent/configuration/sqlite/SqliteProperties.java
@@ -0,0 +1,27 @@
+package au.org.aodn.oceancurrent.configuration.sqlite;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "sqlite")
+public class SqliteProperties {
+
+    private String remoteUrl;
+    private String localPath;
+    private Download download = new Download();
+
+    @Data
+    public static class Download {
+        private Cron cron = new Cron();
+        private int connectTimeout = 30000;
+        private int readTimeout = 60000;
+
+        @Data
+        public static class Cron {
+            private String expression = "0 0 * * * ?"; // every hour
+        }
+    }
+}
